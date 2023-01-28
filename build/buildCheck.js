26d43cf0282fb47c94abc6c3a1552e0f72d1398c
yakisova41 <yakisoba5emon@gmail.com>
2023-01-28 16:05:47 +0000

    buildのinstallをチェックするようにした

--- build/buildCheck.js
+++ build/buildCheck.js
@@ -2,6 +2,10 @@ const fs = require("fs");
 const path = require("path");
 const { exec } = require("child_process");
 
+if (!fs.existsSync(path.join(__dirname, "node_modules"))) {
+    exec("npm install");
+}
+
 if (!fs.existsSync(path.join(__dirname, "dist"))) {
     exec("npx tsc");
 }
