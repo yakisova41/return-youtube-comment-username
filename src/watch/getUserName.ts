72ca8c63b29130b74e1465daa052664291d123a1
yakisova41 <yakisoba5emon@gmail.com>
2023-02-19 18:09:48 +0000

    ⚡️ change api url to cloudflare pages

--- src/watch/getUserName.ts
+++ src/watch/getUserName.ts
@@ -2,11 +2,10 @@ export async function getUserName(href: string) {
     const id = href.split("/")[4];
 
     const data = await fetch(
-        `https://yt-returnname-api.vercel.app/api/idToName/${id}`,
+        `https://yt-returnname-api.pages.dev/api/idToName?id=${id}`,
         {
             method: "POST",
         }
     ).then((res) => res.text());
-
     return data;
 }
