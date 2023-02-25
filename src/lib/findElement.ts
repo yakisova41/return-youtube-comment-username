6af0004fc5bbbee2c1a839ab7da6cba77d01ccf1
yakisova41 <yakisoba5emon@gmail.com>
2023-02-25 04:07:26 +0000

    :zap:  破壊的で互換性を一切考慮しない他人のスクリプトへの互換性対応
    WebAPIを挙動も維持せず書き換えるとか個人的にありえないと思うんだが

--- src/lib/findElement.ts
+++ src/lib/findElement.ts
@@ -4,18 +4,36 @@ export const findElement = (
 ): Promise<Element> => {
     return new Promise((resolve, reject) => {
         let i = 0;
-        const interval = setInterval(() => {
-            const elem = document.querySelector(selector);
-            if (elem !== null) {
-                clearInterval(interval);
-                resolve(elem);
+
+        if (isNativeInterval()) {
+            const interval = setInterval(() => {
+                const elem = document.querySelector(selector);
+                if (elem !== null) {
+                    clearInterval(interval);
+                    resolve(elem);
+                }
+                if (limit < i) {
+                    clearInterval(interval);
+                    reject(null);
+                }
+                i = i + 1;
+            });
+        } else {
+            function search() {
+                setTimeout(() => {
+                    const elem = document.querySelector(selector);
+                    if (elem !== null) {
+                        resolve(elem);
+                    } else if (limit < i) {
+                        reject(null);
+                    } else {
+                        search();
+                    }
+                    i = i + 1;
+                });
             }
-            if (limit < i) {
-                clearInterval(interval);
-                reject(null);
-            }
-            i = i + 1;
-        });
+            search();
+        }
     });
 };
 
@@ -25,17 +43,42 @@ export const findElementAll = (
 ): Promise<NodeListOf<Element>> => {
     return new Promise((resolve, reject) => {
         let i = 0;
-        const interval = setInterval(() => {
-            const elems = document.querySelectorAll(selector);
-            if (elems.length !== 0) {
-                clearInterval(interval);
-                resolve(elems);
+
+        if (isNativeInterval()) {
+            const interval = setInterval(() => {
+                const elems = document.querySelectorAll(selector);
+                if (elems.length !== 0) {
+                    clearInterval(interval);
+                    resolve(elems);
+                }
+                if (limit < i) {
+                    clearInterval(interval);
+                    reject([]);
+                }
+                i = i + 1;
+            });
+        } else {
+            function search() {
+                setTimeout(() => {
+                    const elems = document.querySelectorAll(selector);
+                    if (elems.length !== 0) {
+                        resolve(elems);
+                    } else if (limit < i) {
+                        reject(null);
+                    } else {
+                        search();
+                    }
+                    i = i + 1;
+                });
             }
-            if (limit < i) {
-                clearInterval(interval);
-                reject([]);
-            }
-            i = i + 1;
-        });
+            search();
+        }
     });
 };
+
+function isNativeInterval() {
+    return (
+        window.setInterval.toString() ===
+        "function setInterval() { [native code] }"
+    );
+}
