46a997fffa66798c517a85ddf9e13a1d844a2a91
yakisova41 <yakisoba5emon@gmail.com>
2026-03-26 02:59:43 +0900

    remove "V2" from highlited reply

--- src/rewrites/highlightedReply.ts
+++ src/rewrites/highlightedReply.ts
@@ -1,36 +1,42 @@
 import {
   findElementByTrackingParams,
   reSearchElement,
-  type ShadyElement,
 } from "src/utils/findElementByTrackingParams";
-import { reWriteReplyElemV2 } from "./reply";
+import { reWriteReplyElem } from "./reply";
+import { CommentViewModelElement } from "./rewriteOfCommentRenderer/nameRewriteOfCommentViewModel";
 
 /**
  * highLightedReplyの要素を書き換え
  */
 export function rewriteHighlightedReply(trackedParams: string): void {
   getReplyElem(trackedParams).then((replyElem) => {
-    reWriteReplyElemV2(replyElem);
+    reWriteReplyElem(replyElem);
   });
 }
 
 /**
  * teaser!! リプライの要素をtrackingParamsから取得
  */
-async function getReplyElem(trackedParams: string): Promise<ShadyElement> {
+async function getReplyElem(
+  trackedParams: string,
+): Promise<CommentViewModelElement> {
   return await new Promise((resolve) => {
     const selector =
       "ytd-comment-replies-renderer > #teaser-replies > ytd-comment-view-model";
 
-    const commentRenderer = findElementByTrackingParams<ShadyElement>(
-      trackedParams,
-      selector,
-    );
+    const commentRenderer =
+      findElementByTrackingParams<CommentViewModelElement>(
+        trackedParams,
+        selector,
+      );
 
     if (commentRenderer !== null) {
       resolve(commentRenderer);
     } else {
-      void reSearchElement(trackedParams, selector).then((commentRenderer) => {
+      void reSearchElement<CommentViewModelElement>(
+        trackedParams,
+        selector,
+      ).then((commentRenderer) => {
         resolve(commentRenderer);
       });
     }
